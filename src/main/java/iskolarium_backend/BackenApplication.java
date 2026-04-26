02d84cae32c80297d96903ccd0cc3193cc07d67b
sidrick enriquez <sidrickenriquez@sids-macbook.local>
2026-04-26 14:26:16 +0800

    file updated

--- src/main/java/iskolarium_backend/BackenApplication.java
+++ src/main/java/iskolarium_backend/BackenApplication.java
@@ -2,19 +2,11 @@ package iskolarium_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class BackenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackenApplication.class, args);
-	}
-@SpringBootApplication
-@EnableScheduling // <-- FLIP THE SWITCH ON!
-public class IskolariumBackendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(IskolariumBackendApplication.class, args);
+        SpringApplication.run(BackenApplication.class, args);
     }
-}
 }
