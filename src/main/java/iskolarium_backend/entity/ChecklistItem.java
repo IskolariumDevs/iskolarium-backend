c361259521dbc49376f0a2776459866d649092c7
sidrick enriquez <sidrickenriquez@sids-macbook.local>
2026-05-08 01:25:32 +0800

    grr

--- src/main/java/iskolarium_backend/entity/ChecklistItem.java
+++ src/main/java/iskolarium_backend/entity/ChecklistItem.java
@@ -1,6 +1,17 @@
 package iskolarium_backend.entity;
 
-import jakarta.persistence.*;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
@@ -27,4 +38,7 @@ public class ChecklistItem {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setSubmitted'");
     }
+    @OneToMany(mappedBy = "tracker", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChecklistItem> checklistItems;
+
 }
